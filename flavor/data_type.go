6b825c1296b43ab421d760fd347a118a880834ef
Andrew Arrow <oneone@gmail.com>
2022-09-25 08:27:43 -0700

    data types

---
+++ flavor/data_type.go
@@ -0,0 +1,39 @@
+package flavor
+
+func DataType(flavor string) string {
+	val := ""
+	if flavor == "address" {
+		val = "string"
+	} else if flavor == "latitude" {
+		val = "string"
+	} else if flavor == "longitude" {
+		val = "string"
+	} else if flavor == "timestamp" {
+		val = "string"
+	} else if flavor == "few_words" {
+		val = "string"
+	} else if flavor == "float" {
+		val = "float64"
+	} else if flavor == "int" {
+		val = "int"
+	} else if flavor == "paragraph" {
+		val = "string"
+	} else if flavor == "first_name" {
+		val = "string"
+	} else if flavor == "last_name" {
+		val = "string"
+	} else if flavor == "pronouns" {
+		val = "string"
+	} else if flavor == "email" {
+		val = "string"
+	} else if flavor == "phone" {
+		val = "string"
+	} else if flavor == "uuid" {
+		val = "string"
+	} else if flavor == "bool" {
+		val = "bool"
+	} else if flavor == "model" {
+		val = "string"
+	}
+	return val
+}
