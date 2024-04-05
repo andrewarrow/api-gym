11f5e45803b97f79ba70320d5a67b1dc9233f5f5
Andrew Arrow <oneone@gmail.com>
2024-04-05 14:23:56 -0700

    Fix bug in main path

--- browser/endpoints.go
+++ browser/endpoints.go
@@ -1,16 +1,20 @@
 package browser
 
+var routeById map[string]*Route
+
 type Route struct {
 	Id   string
 	Open bool
 }
 
 func RegisterEndpoints() {
+	routeById = map[string]*Route{}
 	//Document.Document.Call("addEventListener", "keydown", js.FuncOf(g.GymKeyPress))
 	div := Document.ByIdWrapped("routes")
 	for _, input := range div.SelectAllByClass("cursor-pointer") {
 		route := Route{}
 		route.Id = input.Id[2:]
+		routeById[route.Id] = &route
 		input.EventWithId(route.Click)
 	}
 }
@@ -21,7 +25,11 @@ func (r *Route) Click() {
 	div := Document.ByIdWrapped("routes")
 	for _, input := range div.SelectAllByClass("cursor-pointer") {
 		id := input.Id[2:]
+		if r.Id == id {
+			continue
+		}
 		Document.Id("r" + id).AddClass("hidden")
+		routeById[id].Open = false
 	}
 	if r.Open == false {
 		Document.Id("r" + r.Id).RemoveClass("hidden")
